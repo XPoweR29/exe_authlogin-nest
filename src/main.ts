ec0f97a86b91403eddb64702981c443efb5a30d4
XPoweR29 <xpower-29@wp.pl>
2023-06-08 19:21:58 +0200

    Enabled CORS for frontend application

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     disableErrorMessages: false,
     whitelist: true,
